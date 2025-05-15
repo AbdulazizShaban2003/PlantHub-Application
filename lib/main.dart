d9379861b85e1f8c5f5db6c739477a9f097f495c
abdulaziz shaban abdualziz <abdulazizshaban9860@gmail.com>
2025-05-15 23:01:22 +0300

    related firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app/my_app.dart';
 import 'core/cache/cache_helper.dart';
 import 'core/service/service_locator.dart';
+import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setup();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await sl<CacheHelper>().init();
   await EasyLocalization.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
