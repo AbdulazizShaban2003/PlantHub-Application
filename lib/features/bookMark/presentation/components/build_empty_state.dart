8f82b9f9af081156be9ea7f7c57c680ca7eba8fa
abdulaziz shaban abdulaziz <abdelazizs983@gmail.com>
2025-06-10 07:45:31 +0300

    add divider in bookmark

---
+++ lib/features/bookMark/presentation/components/build_empty_state.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../../../../config/theme/app_colors.dart';
+import '../../../../core/utils/app_strings.dart';
+import '../../../../core/utils/size_config.dart' show SizeConfig;
+
+Widget buildEmptyState(BuildContext context) {
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.bookmark_border,
+          size: SizeConfig().responsiveFont(48),
+          color: ColorsManager.greyColor,
+        ),
+        SizedBox(height: SizeConfig().height(0.02)),
+        Text(
+          AppStrings.noBookmarksTitle,
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+            fontSize: SizeConfig().responsiveFont(18),
+          ),
+        ),
+        SizedBox(height: SizeConfig().height(0.01)),
+        Text(
+          AppStrings.noBookmarksSubtitle,
+          style: Theme.of(context).textTheme.bodySmall?.copyWith(
+            color: ColorsManager.greyColor,
+            fontSize: SizeConfig().responsiveFont(14),
+          ),
+        ),
+      ],
+    ),
+  );
+}
--- lib/features/bookMark/presentation/views/bookmark_view.dart
+++ lib/features/bookMark/presentation/views/bookmark_view.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:plant_hub_app/config/theme/app_colors.dart';
 import 'package:plant_hub_app/core/utils/app_strings.dart';
 import 'package:plant_hub_app/core/utils/size_config.dart';
 import 'package:plant_hub_app/features/articles/view_model.dart'
     show PlantViewModel;
 import '../../../articles/data/models/plant_model.dart';
+import '../components/build_empty_state.dart' show buildEmptyState;
 import '../widgets/access_login_bookmark.dart';
 import '../widgets/build_error_widget.dart';
 import '../widgets/build_plant_article_card.dart';
@@ -24,41 +24,12 @@ class _BookmarkViewState extends State<BookmarkView> {
     await widget.plantProvider.fetchAllPlants();
   }
 
-  Widget _buildEmptyState() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.bookmark_border,
-            size: SizeConfig().responsiveFont(48),
-            color: ColorsManager.greyColor,
-          ),
-          SizedBox(height: SizeConfig().height(0.02)),
-          Text(
-            AppStrings.noBookmarksTitle,
-            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-              fontSize: SizeConfig().responsiveFont(18),
-            ),
-          ),
-          SizedBox(height: SizeConfig().height(0.01)),
-          Text(
-            AppStrings.noBookmarksSubtitle,
-            style: Theme.of(context).textTheme.bodySmall?.copyWith(
-              color: ColorsManager.greyColor,
-              fontSize: SizeConfig().responsiveFont(14),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 
   Widget _buildPlantList(List<Plant> bookmarkedPlants) {
     return RefreshIndicator(
       onRefresh: _refreshData,
       child: ListView.builder(
-        padding: EdgeInsets.all(SizeConfig().width(0.06)),
+        padding: EdgeInsets.all(SizeConfig().width(0.6)),
         itemCount: bookmarkedPlants.length,
         itemBuilder: (context, index) {
           final plant = bookmarkedPlants[index];
@@ -93,22 +64,29 @@ class _BookmarkViewState extends State<BookmarkView> {
           child: Icon(Icons.close, size: SizeConfig().responsiveFont(35)),
         ),
       ),
-      body: StreamBuilder<List<Plant>>(
-        stream: widget.plantProvider.getBookmarkedPlantsStream(context),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Column(
+        children: [
+          Divider(),
+          Expanded(
+            child: StreamBuilder<List<Plant>>(
+              stream: widget.plantProvider.getBookmarkedPlantsStream(context),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
 
-          if (snapshot.hasError) {
-            return buildErrorWidget(snapshot.error!, context, _refreshData);
-          }
+                if (snapshot.hasError) {
+                  return buildErrorWidget(snapshot.error!, context, _refreshData);
+                }
 
-          final bookmarkedPlants = snapshot.data ?? [];
-          return bookmarkedPlants.isEmpty
-              ? _buildEmptyState()
-              : _buildPlantList(bookmarkedPlants);
-        },
+                final bookmarkedPlants = snapshot.data ?? [];
+                return bookmarkedPlants.isEmpty
+                    ? buildEmptyState(context)
+                    : _buildPlantList(bookmarkedPlants);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
