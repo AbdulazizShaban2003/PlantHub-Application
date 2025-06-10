ff9d0c23de37ccbc1263ce86e237bd1e7c1a864c
abdulaziz shaban abdulaziz <abdelazizs983@gmail.com>
2025-06-10 09:59:49 +0300

    remove some librais and images

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:provider/provider.dart';
 import 'app/my_app.dart';
 import 'core/cache/cache_helper.dart';
